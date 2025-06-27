08d0c4e8361dc286ac5431c8ad369da6a1d64508
ChrisG <chisven97@gmail.com>
2025-06-27 17:21:08 -0500

    Parcial final CSS y HTM

---
+++ main.js
@@ -0,0 +1,102 @@
+const ListaProductos = [
+
+    {
+        id: "01",
+        titulo: "Rascador de carton corrugado",
+        Imagen: ".\PurrfectShop\Item1-Rascador.png",
+        categoria: {
+            nombre: "Juguetes",
+            id: "Juguetes"
+        },
+        precio: 12.99
+
+    },
+    {
+        id: "02",
+        titulo: "Juguete interactivo con plumas",
+        Imagen: ".\PurrfectShop\jugueteplumas.jpg",
+        categoria: {
+            nombre: "Juguetes",
+            id: "Juguetes"
+        },
+        precio: 17.50
+
+    },
+    {
+        id: "03",
+        titulo: "Fuente de agua automatica",
+        Imagen: ".\PurrfectShop\fuete item 3.webp",
+        categoria: {
+            nombre: "Fuentes",
+            id: "Fuentes"
+        },
+        precio: 28.99
+
+    },
+    {
+        id: "04",
+        titulo: "Cama cueva",
+        Imagen: ".\PurrfectShop\camacueva.jpg",
+        categoria: {
+            nombre: "Camas",
+            id: "Camas"
+        },
+        precio: 24.95
+
+    },
+    {
+        id: "05",
+        titulo: "Arenero cerrado con filtro",
+        Imagen: ".\PurrfectShop\arenero2.webp",
+        categoria: {
+            nombre: "Areneros",
+            id: "Areneros"
+        },
+        precio: 39.99
+
+    },
+    {
+        id: "06",
+        titulo: "Torre de juegos",
+        Imagen: "torre_gato6.jpg",
+        categoria: {
+            nombre: "Juguetes",
+            id: "Juguetes"
+        },
+        precio: 198.50
+
+    },
+    {
+        id: "01",
+        titulo: "Juguete interactivo con plumas",
+        Imagen: ".\PurrfectShop\jugueteplumas.jpg",
+        categoria: {
+            nombre: "Juguetes",
+            id: "Juguetes"
+        },
+        precio: 17.50
+
+    },
+    {
+        id: "01",
+        titulo: "Juguete interactivo con plumas",
+        Imagen: ".\PurrfectShop\jugueteplumas.jpg",
+        categoria: {
+            nombre: "Juguetes",
+            id: "Juguetes"
+        },
+        precio: 17.50
+
+    },
+    {
+        id: "01",
+        titulo: "Juguete interactivo con plumas",
+        Imagen: ".\PurrfectShop\jugueteplumas.jpg",
+        categoria: {
+            nombre: "Juguetes",
+            id: "Juguetes"
+        },
+        precio: 17.50
+
+    },
+];
